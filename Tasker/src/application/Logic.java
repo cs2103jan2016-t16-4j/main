c3a9012be13b0cfa3a9590e0534119d82f34c386
shawnlimjq <shawnlimjq@hotmail.com>
2016-02-19 23:26:20 +0800

    Logic.java File Created with some skeleton methods
    Logic java file created with processCommand, showFeedback, add, undo
    skeleton methods

---
+++ Tasker/src/application/Logic.java
@@ -0,0 +1,60 @@
+package application;
+
+public class Logic {
+
+	private String previousCommand;
+	private String feedBack;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+//	private void processCommand(String cmd){
+//		String[] command = Parser.getCommands(cmd);
+//		switch (command[0]){
+//			case "add":
+//			previousCommand = cmd;
+//					task = add(command[1]);
+//					feedBack = Storage.add(task);
+//					showfeedback(feedBack);
+//			break;
+//			case "delete":
+//			previousCommand = cmd;
+//					showfeedback(feedBack);
+//			break;
+//			case "search":
+//			previousCommand = cmd;
+//					showfeedback(feedBack);
+//			break;
+//			case "update":
+//			previousCommand = cmd;
+//			showfeedback(feedBack);
+//			break;
+//			case "exit":
+//					showfeedback(feedBack);
+//			System.exit(0);
+//			break;
+//			case "undo":
+//			undo();
+//			case default:
+//			previousCommand = cmd;
+//					task = add(command[1]);
+//					Storage.add(task);
+//					showfeedback(feedBack);
+//		}
+//	}
+//
+//	private String showFeedback(String feedback) {
+//		UI.displayFeedback();
+//	}
+//
+//	private Task add(String message) {
+//		Parser.decipherAdd(message);
+//	}
+//
+//	private void undo() {
+//		Parser.Storage.undo();
+//	}
+
+}
