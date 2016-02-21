f8af0bd783f5239aa3eb634eeb622989b8a0d5bf
shawnlimjq <shawnlimjq@hotmail.com>
2016-02-21 16:29:59 +0800

    Shift application.css to the GUI folder and Refactored GUI.java

--- Tasker/src/application/GUI/GUI.java
+++ Tasker/src/application/GUI/GUI.java
@@ -8,8 +8,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class GUI extends Application {
@@ -18,27 +16,44 @@ public class GUI extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			ObservableList<String> data = FXCollections.observableArrayList();
-
-		    ListView<String> listView = new ListView<String>(data);
-		    listView.setPrefSize(900, 900);
-		    data.addAll("A", "B", "C", "D", "E");
+			ListView<String> listView = createList();
 
 			setProgramName(primaryStage);
-			ScrollPane scrollPane = new ScrollPane();
-			scrollPane.setContent(listView);
-			TextField titleTextField;
-			titleTextField = new TextField();
-			VBox cli = new VBox();
-			cli.getChildren().addAll(scrollPane,titleTextField);
-			Scene scene = new Scene(cli, 800, 800);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			ScrollPane scrollPane = addToScrollPane(listView);
+			TextField titleTextField = new TextField();
+			VBox cli = addToVBox(scrollPane, titleTextField);
+			setStage(primaryStage, cli);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	private void setStage(Stage primaryStage, VBox cli) {
+		Scene scene = new Scene(cli, 800, 800);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	private VBox addToVBox(ScrollPane scrollPane, TextField titleTextField) {
+		VBox cli = new VBox();
+		cli.getChildren().addAll(scrollPane, titleTextField);
+		return cli;
+	}
+
+	private ScrollPane addToScrollPane(ListView<String> listView) {
+		ScrollPane scrollPane = new ScrollPane();
+		scrollPane.setContent(listView);
+		return scrollPane;
+	}
+
+	private ListView<String> createList() {
+		ObservableList<String> data = FXCollections.observableArrayList();
+
+		ListView<String> listView = new ListView<String>(data);
+		listView.setPrefSize(900, 900);
+		data.addAll("A", "B", "C", "D", "E");
+		return listView;
 	}
 
 	private void setProgramName(Stage primaryStage) {
