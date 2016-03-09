ff81e16966b2c0976b5224fc781608b8c3f8c046
Ruiming78 <a0078688@gmail.com>
2016-03-10 00:14:16 +0800

    Changing Done and  Help

--- Tasker/src/application/parser/DoneByName.java
+++ Tasker/src/application/parser/DoneByName.java
@@ -1,20 +1,62 @@
 package application.parser;
 
 import application.storage.Storage;
+import application.storage.Task;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class DoneByName implements Command {
+     private static final int FIRST_INDEX = 0;
+    private static final String FEEDBACK_CLOSE = "CLOSE Task: %1$s";
+    private static final String MESSAGE_CLOSE_ERROR = "We encountered some "
+            + "problem while closing this task. We apologise for the inconvenience.";
     
+    
+  
     String taskToClose;
     
     DoneByName(String taskToClose){
         this.taskToClose = taskToClose;
+        
+        
     }
      
     //NEED TO IMPLEMENT THIS
     public String execute(Storage storage){
-        return "FEEDBACK FOR DONE BY NAME";
+         try{
+            ArrayList<Task> taskList = storage.searchByTask(taskToClose);
+            String feedback = takeAction(taskList, storage);
+            return feedback;
+        }catch(IOException e){
+            return MESSAGE_CLOSE_ERROR;
+        }
     }
     
+    public String takeAction(ArrayList<Task> taskList, Storage storage) throws IOException{
+        assert(taskList.size() > 0);
+        if (taskList.size() ==  1){
+            return closeSingleTask(taskList, storage);
+        } else {
+            return listTasks(taskList);
+        }
+    }
+
+    private String closeSingleTask(ArrayList<Task> taskList, Storage storage) throws IOException {
+        String feedback = String.format(FEEDBACK_CLOSE, 
+                taskList.get(FIRST_INDEX).getTaskDescription());
+        storage.closeTaskInList(FIRST_INDEX);
+        return feedback;
+    }
+    
+    private String listTasks(ArrayList<Task> taskList){
+        String listed = "\nWhich task would you like to close?";
+        int i = 1;
+        for (Task task: taskList){
+            listed = listed + "\n" + i + ") " + task.getTaskDescription();
+            i++;
+        }
+        return listed;
+    }
     
     
     
--- Tasker/src/application/parser/Help.java
+++ Tasker/src/application/parser/Help.java
@@ -5,7 +5,46 @@ import application.storage.Storage;
 public class Help implements Command {
 	
 	public String execute(Storage storage) {
-		return "HELP MESSAGE";
+          String msg=String.format("%30s", "add");
+          msg=msg+"    --Adds new tasks (keyword add is not required)\n";
+          msg=msg+String.format("%30s", " ");
+          msg=msg+"    --Adds <Task> due by <date> at <Venue> and sets reminder\n";
+          msg=msg+String.format("%30s", " ");
+          msg=msg+"    --Note: by <Date>, @ <Venue>, remind <When>, priority <Level> are optional.\n";
+          msg=msg+String.format("%30s", " ");
+          msg=msg+"    --Note: Use “every” instead of “by” to add recurring tasks.\n";
+          
+          msg=msg+String.format("%30s", "search");
+          msg=msg+"    --Search for tasks\n";
+          msg=msg+String.format("%30s", " ");
+          msg=msg+"    --Search by <Name> or priority <Level> or due <Date>\n";
+          
+          msg=msg+String.format("%30s", "delete");
+          msg=msg+"    --Delete tasks\n";
+          msg=msg+String.format("%30s", " ");
+          msg=msg+"    --Deletes tasks by name or number assigned to the task\n";  
+          
+          msg=msg+String.format("%30s", "update");
+          msg=msg+"    --Updates a task\n";
+          msg=msg+String.format("%30s", " ");
+          msg=msg+"    --Updates task to change description or details.\n";           
+          msg=msg+String.format("%30s", " ");
+          msg=msg+"    --Note: Able to use keywords such as by/at/remind/priority to change the other details also\n"; 
+          
+          msg=msg+String.format("%30s", "closed");
+          msg=msg+"    --Closes/Marks a completed task.\n";
+          msg=msg+String.format("%30s", " ");
+          msg=msg+"    --Marks <task> as completed.\n"; 
+          
+          msg=msg+String.format("%30s", "undo");
+          msg=msg+"    --Undoes the last input\n";
+          
+          msg=msg+String.format("%30s", "storage");
+          msg=msg+"    --Changes the storage location\n";
+          
+          msg=msg+String.format("%30s", "help");
+          msg=msg+"    --Shows help screen for commands.\n";
+          return msg;
 	}
 
 }
