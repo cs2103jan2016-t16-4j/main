49f67d5a06980209950c74f43d11da504a3a866e
Pratyush More <Pratyush More>
2016-03-30 23:53:53 +0800

    Fixed closed tasks show

--- Tasker/src/application/logic/ShowDoneTasks.java
+++ Tasker/src/application/logic/ShowDoneTasks.java
@@ -7,12 +7,20 @@ import application.storage.Task;
 
 public class ShowDoneTasks implements Command{
 
-    private static final String CLOSED_MESSAGE = "Here are the tasks you have closed.";
+    private static final String MESSAGE_CLOSED = "Here are the tasks you have closed.";
+    private static final String MESSAGE_NO_CLOSED = "You have not closed any tasks.";
     
     @Override
     public Feedback execute(Storage storage, ArrayList<Task> tasksOnScreen) {
-        ArrayList<Task> tasks = storage.getClosedList();
-        return new Feedback(CLOSED_MESSAGE, tasks);
+        ArrayList<Task> tasks = storage.getCloseList();
+        return createAppropFeedback(storage, tasks);
     }
     
+    private static Feedback createAppropFeedback(Storage storage, ArrayList<Task> tasks){
+        if (tasks.size() == 0){
+            return new Feedback(MESSAGE_NO_CLOSED, storage.getOpenList());
+        } else{
+            return new Feedback(MESSAGE_CLOSED, tasks);
+        }
+    }
 }
