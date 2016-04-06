e3e1cb35a02766336b0f1bb8b23589134f2f6b73
shawnlimjq <shawnlimjq@hotmail.com>
2016-04-06 22:56:40 +0800

    Set Color According to Priority for Calendar View

--- Tasker/src/application/gui/CalendarItem.java
+++ Tasker/src/application/gui/CalendarItem.java
@@ -7,9 +7,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
 
 public class CalendarItem extends AnchorPane {
 	public static final String EMPTY = "";
+	public static final String RED = "#EF9A9A";
+	public static final String GREEN = "#A5D6A7";
+	public static final String BLUE = "#B2EBF2";
+	public static final String BACKGROUND_COLOR = "-fx-fill: %1$s;";
 
 	@FXML
 	private Label taskName;
@@ -17,14 +22,16 @@ public class CalendarItem extends AnchorPane {
 	private Label date;
 	@FXML
 	private Label locationLabel;
+	@FXML
+	private Rectangle rectangle;
 
-	public CalendarItem(String name, String date, String location) {
+	public CalendarItem(String name, String date, String location, String priority) {
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("CalendarItem.fxml"));
 		try {
 			fxmlLoader.setRoot(this);
 			fxmlLoader.setController(this);
 			fxmlLoader.load();
-			this.setLabels(name, date, location);
+			this.setLabels(name, date, location, priority);
 
 		} catch (IOException exception) {
 			System.out.println("Could not load");
@@ -33,10 +40,29 @@ public class CalendarItem extends AnchorPane {
 
 	}
 
-	private void setLabels(String name, String date, String location) {
+	private void setLabels(String name, String date, String location, String priority) {
 		this.taskName.setText(name.toUpperCase());
 		this.date.setText(date.toUpperCase());
 		setLocation(location.trim().toUpperCase());
+		setPriorityColor(priority);
+	}
+
+	private void setPriorityColor(String priority) {
+		switch (priority) {
+		case "high":
+			rectangle.setStyle(String.format(BACKGROUND_COLOR, RED));
+			break;
+		case "medium":
+			rectangle.setStyle(String.format(BACKGROUND_COLOR, BLUE));
+			break;
+		case "low":
+			rectangle.setStyle(String.format(BACKGROUND_COLOR, GREEN));
+			break;
+		default:
+			rectangle.setStyle(String.format(BACKGROUND_COLOR, GREEN));
+			break;
+		}
+
 	}
 
 	private void setLocation(String location) {
--- Tasker/src/application/gui/ListItem.java
+++ Tasker/src/application/gui/ListItem.java
@@ -2,12 +2,8 @@
 package application.gui;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 
@@ -58,6 +54,9 @@ public class ListItem extends HBox {
 			listNumber.setStyle(String.format(BACKGROUND_STYLE, BLUE));
 			break;
 		case "low":
+			listNumber.setStyle(String.format(BACKGROUND_STYLE, GREEN));
+			break;
+		default:
 			listNumber.setStyle(String.format(BACKGROUND_STYLE, GREEN));
 			break;
 		}
