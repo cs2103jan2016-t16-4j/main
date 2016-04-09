041b5014045f6d5a4e69a62c71621f03f4494f22
g4rald <gerald_twk@hotmail.com>
2016-04-09 20:09:33 +0800

    Added comments for Storage #1

--- Tasker/src/application/storage/EventTask.java
+++ Tasker/src/application/storage/EventTask.java
@@ -3,11 +3,18 @@
 package application.storage;
 
 import java.util.Calendar;
-
-public class EventTask extends Task implements Cloneable{
+/**
+ * EventTask is a subclass of Task class. 
+ * This class is used when the user creates a task with a event duration. 
+ * It has an two extra parameter, startDate and endDate, to denote the task duration.
+ */
+public class EventTask extends Task implements Cloneable {
 	private Calendar startDate;
 	private Calendar endDate;
-	
+
+	/**
+	 * Creates a EventTask object with "empty" variables initalised.
+	 */
 	public EventTask() {
 		setTaskDescription(EMPTY_STRING);
 		startDate = Calendar.getInstance();
@@ -18,8 +25,10 @@ public class EventTask extends Task implements Cloneable{
 		setTaskIndex(EMPTY_TASK);
 	}
 	
-	public EventTask(String taskDescription, Calendar startDate,
-			Calendar endDate, String location, Calendar remindDate,
+	/**
+	 * Creates a EventTask object with specified variables.
+	 */
+	public EventTask(String taskDescription, Calendar startDate, Calendar endDate, String location, Calendar remindDate,
 			String priority, int taskIndex) {
 		setTaskDescription(taskDescription);
 		this.startDate = startDate;
@@ -29,45 +38,72 @@ public class EventTask extends Task implements Cloneable{
 		setPriority(priority);
 		setTaskIndex(taskIndex);
 	}
-	
+
+	/**
+	 * Set the start date (year, month, date).
+	 */
 	public void setStartDate(Calendar startDate) {
 		this.startDate.set(startDate.get(Calendar.YEAR), startDate.get(Calendar.MONTH), startDate.get(Calendar.DATE));
 	}
 	
+	/**
+	 * Set the start time (hour, minute, seconds, milliseconds).
+	 */
 	public void setStartTime(Calendar startTime) {
-	    this.startDate.set(Calendar.HOUR_OF_DAY, startTime.get(Calendar.HOUR_OF_DAY));
+		this.startDate.set(Calendar.HOUR_OF_DAY, startTime.get(Calendar.HOUR_OF_DAY));
 		this.startDate.set(Calendar.MINUTE, startTime.get(Calendar.MINUTE));
-	    this.startDate.set(Calendar.SECOND, startTime.get(Calendar.SECOND));
+		this.startDate.set(Calendar.SECOND, startTime.get(Calendar.SECOND));
 		this.startDate.set(Calendar.MILLISECOND, startTime.get(Calendar.MILLISECOND));
 	}
 
+	/**
+	 * Set the end date (year, month, date).
+	 */
 	public void setEndDate(Calendar endDate) {
 		this.endDate.set(endDate.get(Calendar.YEAR), endDate.get(Calendar.MONTH), endDate.get(Calendar.DATE));
 	}
 
+	/**
+	 * Set the end time (hour, minute, seconds, milliseconds).
+	 */
 	public void setEndTime(Calendar endTime) {
 		this.endDate.set(Calendar.HOUR_OF_DAY, endTime.get(Calendar.HOUR_OF_DAY));
 		this.endDate.set(Calendar.MINUTE, endTime.get(Calendar.MINUTE));
 		this.endDate.set(Calendar.SECOND, endTime.get(Calendar.SECOND));
 		this.endDate.set(Calendar.MILLISECOND, endTime.get(Calendar.MILLISECOND));
 	}
-	
+
+	/**
+	 * Returns the start date.
+	 */
 	public Calendar getStartDate() {
 		return startDate;
 	}
-	
+
+	/**
+	 * Returns the start time.
+	 */
 	public Calendar getStartTime() {
 		return startDate;
 	}
-
+	
+	/**
+	 * Returns the end date.
+	 */
 	public Calendar getEndDate() {
 		return endDate;
 	}
 
+	/**
+	 * Returns the end time.
+	 */
 	public Calendar getEndTime() {
 		return endDate;
 	}
-	
+
+	/**
+	 * Returns the event duration in String.
+	 */
 	public String durationToString() {
 		String durationMessage = EMPTY_STRING;
 		String startingDate = dateToString(startDate);
@@ -79,19 +115,18 @@ public class EventTask extends Task implements Cloneable{
 			durationMessage += startingDate;
 			if (!startingTime.equals(EMPTY_TIME_STRING)) {
 				durationMessage += KEYWORD_SPACE + startingTime;
-			} // if start date and end date is same, display 1 date + both time only
+			} // if start date and end date is same, display same date + both time
 			if (startingDate.equalsIgnoreCase(endingDate)) {
 				if (startingTime.equals(EMPTY_TIME_STRING)) {
 					durationMessage += endingTime;
-				}
-				else if (!endingTime.equals(EMPTY_TIME_STRING) && !startingTime.equals(EMPTY_TIME_STRING)) {
+				} else if (!endingTime.equals(EMPTY_TIME_STRING) && !startingTime.equals(EMPTY_TIME_STRING)) {
 					durationMessage += KEYWORD_TO + endingTime;
 				}
 				return durationMessage;
 			}
 		}
 		// display start date and end date, if both exist
-		if ( (!startingDate.equals(EMPTY_DATE_STRING)) && (!endingDate.equals(EMPTY_DATE_STRING)) ) {
+		if ((!startingDate.equals(EMPTY_DATE_STRING)) && (!endingDate.equals(EMPTY_DATE_STRING))) {
 			durationMessage += KEYWORD_TO + endingDate;
 			if (!endingTime.equals(EMPTY_TIME_STRING)) {
 				durationMessage += KEYWORD_SPACE + endingTime;
@@ -102,56 +137,60 @@ public class EventTask extends Task implements Cloneable{
 				durationMessage += KEYWORD_SPACE + endingTime;
 			}
 		}
-		return durationMessage;		
+		return durationMessage;
 	}
 	
+	/**
+	 * Returns the task details in String.
+	 */
 	public String toString() {
 		String taskDetails = KEYWORD_QUOTE;
 		taskDetails += getTaskDescription();
 		taskDetails += KEYWORD_SPACE + durationToString();
-		
 		if (!getLocation().equalsIgnoreCase(EMPTY_STRING)) {
 			taskDetails += KEYWORD_AT + getLocation();
 		}
-		
 		taskDetails += KEYWORD_QUOTE;
-		
-		return taskDetails;	
+		return taskDetails;
 	}
-	
-    protected Object clone() throws CloneNotSupportedException {
-    	EventTask newTask = (EventTask) super.clone();
-    	newTask.startDate = (Calendar) startDate.clone();
-    	newTask.endDate = (Calendar) endDate.clone();
-    	newTask.remindDate = (Calendar) remindDate.clone();
-    	return newTask;
-    }
-    //@@author A0110422E
-  	public String getPriority() {
-  		Calendar currentTime = Calendar.getInstance();
-  		Calendar startTime = startDate;
-  		String tempPriority = "low";
-  		if (startTime.before(currentTime)) {
-  			setPriority("high");
-  		}
-  		if (priority == "") {
-  			// End time is in less than two hours or before current time
-  			currentTime.roll(Calendar.HOUR, 2);
-  			if (startTime.before(currentTime)) {
-  				tempPriority = "high";
-  			} else {
-  				currentTime.roll(Calendar.HOUR, -2);		
-  				// End time is between two hours and one day
-  				currentTime.roll(Calendar.DATE, 1);
-  				if (startTime.before(currentTime)) {
-  					tempPriority = "medium";
-  					currentTime.roll(Calendar.DATE, -1);
-  				}	
-  			}		
-  			currentTime.roll(Calendar.DATE, -1);	
-  		} else {
-  			tempPriority = priority;
-  		}
-  		return tempPriority;
-  	}    
+
+	/**
+	 * Clones a EventTask.
+	 */
+	protected Object clone() throws CloneNotSupportedException {
+		EventTask newTask = (EventTask) super.clone();
+		newTask.startDate = (Calendar) startDate.clone();
+		newTask.endDate = (Calendar) endDate.clone();
+		newTask.remindDate = (Calendar) remindDate.clone();
+		return newTask;
+	}
+
+	// @@author A0110422E
+	public String getPriority() {
+		Calendar currentTime = Calendar.getInstance();
+		Calendar startTime = startDate;
+		String tempPriority = "low";
+		if (startTime.before(currentTime)) {
+			setPriority("high");
+		}
+		if (priority == "") {
+			// End time is in less than two hours or before current time
+			currentTime.roll(Calendar.HOUR, 2);
+			if (startTime.before(currentTime)) {
+				tempPriority = "high";
+			} else {
+				currentTime.roll(Calendar.HOUR, -2);
+				// End time is between two hours and one day
+				currentTime.roll(Calendar.DATE, 1);
+				if (startTime.before(currentTime)) {
+					tempPriority = "medium";
+					currentTime.roll(Calendar.DATE, -1);
+				}
+			}
+			currentTime.roll(Calendar.DATE, -1);
+		} else {
+			tempPriority = priority;
+		}
+		return tempPriority;
+	}
 }
--- Tasker/src/application/storage/FloatingTask.java
+++ Tasker/src/application/storage/FloatingTask.java
@@ -3,9 +3,15 @@
 package application.storage;
 
 import java.util.Calendar;
+/**
+ * FloatingTask is a subclass of Task class. 
+ * This class is used when the user creates a task with no date specified.
+ */
+public class FloatingTask extends Task implements Cloneable {
 
-public class FloatingTask extends Task implements Cloneable  {
-
+	/**
+	 * Creates a FloatingTask object with "empty" variables initalised.
+	 */
 	public FloatingTask() {
 		setTaskDescription(EMPTY_STRING);
 		setLocation(EMPTY_STRING);
@@ -13,53 +19,73 @@ public class FloatingTask extends Task implements Cloneable  {
 		setPriority(EMPTY_STRING);
 		setTaskIndex(EMPTY_TASK);
 	}
-	
+
+	/**
+	 * Creates a FloatingTask object with specified variables.
+	 */
 	public FloatingTask(String taskDescription, String location, Calendar remindDate, String priority, int taskIndex) {
-		
 		setTaskDescription(taskDescription);
 		setLocation(location);
 		setRemindDate(remindDate);
 		setPriority(priority);
 		setTaskIndex(taskIndex);
 	}
-		
+
+	/**
+	 * Returns null as no start date for FloatingTask.
+	 */
 	public Calendar getStartDate() {
 		return NO_DATE;
 	}
 
+	/**
+	 * Returns null as no end date for FloatingTask.
+	 */
 	public Calendar getEndDate() {
 		return NO_DATE;
 	}
-	
+
+	/**
+	 * Returns null as no start time for FloatingTask.
+	 */
 	public Calendar getStartTime() {
 		return NO_TIME;
 	}
 
+	/**
+	 * Returns null as no end time for FloatingTask.
+	 */
 	public Calendar getEndTime() {
 		return NO_TIME;
 	}
-	
+
+	/**
+	 * Returns empty string as no duration for FloatingTask.
+	 */
 	public String durationToString() {
 		return EMPTY_STRING;
 	}
-	
+
+	/**
+	 * Returns the task details in String.
+	 */
 	public String toString() {
 		String taskDetails = KEYWORD_QUOTE;
 		taskDetails += getTaskDescription();
-		
 		if (!getLocation().equalsIgnoreCase(EMPTY_STRING)) {
 			taskDetails += KEYWORD_AT + getLocation();
 		}
-		
 		taskDetails += KEYWORD_QUOTE;
-		
-		return taskDetails;	
+		return taskDetails;
 	}
-	
-    protected Object clone() throws CloneNotSupportedException {
-    	FloatingTask newTask = (FloatingTask) super.clone();
-    	newTask.remindDate = (Calendar) remindDate.clone();
-    	return newTask;
-    }
+
+	/**
+	 * Clones a FloatingTask.
+	 */
+	protected Object clone() throws CloneNotSupportedException {
+		FloatingTask newTask = (FloatingTask) super.clone();
+		newTask.remindDate = (Calendar) remindDate.clone();
+		return newTask;
+	}
 
 }
