02a58066b4355b179fb8d4c50742d04e7e3136d7
shawnlimjq <shawnlimjq@hotmail.com>
2016-04-10 18:06:06 +0800

    Added in Check for Event Task

--- Tasker/src/application/backend/Logic.java
+++ Tasker/src/application/backend/Logic.java
@@ -12,6 +12,7 @@ import application.storage.Task;
 //@@author A0132632R
 /**
  * This class can be thought of as the decision maker for the backend package.
+ * 
  * @author Pratyush
  *
  */
@@ -27,25 +28,29 @@ public class Logic {
 	private static Logger logger = LoggerHandler.getLog();
 	private History history = History.getInstance();
 
-	public Logic(){
+	public Logic() {
 	}
-	
-	//To enable testing
-	protected Logic (StorageConnector storageConnector){
-	    this.storageConnector = storageConnector;
+
+	// To enable testing
+	protected Logic(StorageConnector storageConnector) {
+		this.storageConnector = storageConnector;
 	}
-	
+
 	/**
 	 * This method should be called to parse and execute user entered commands
-	 *  (which may be in natural language style).
-	 * @param command  A string input of the user command
-	 * @param tasksOnScreen An ArrayList of the current tasks being displayed
-	 * @return A feedback object containing feedback which has been constructed based
-	 *  on the execution of that particular command
-	 * @throws NoDescriptionException This exception is thrown if the Parser encounters 
-	 * that the user has not entered a description for a task
+	 * (which may be in natural language style).
+	 * 
+	 * @param command
+	 *            A string input of the user command
+	 * @param tasksOnScreen
+	 *            An ArrayList of the current tasks being displayed
+	 * @return A feedback object containing feedback which has been constructed
+	 *         based on the execution of that particular command
+	 * @throws NoDescriptionException
+	 *             This exception is thrown if the Parser encounters that the
+	 *             user has not entered a description for a task
 	 */
-	
+
 	public Feedback executeCommand(String command, ArrayList<Task> tasksOnScreen) throws NoDescriptionException {
 		Feedback feedback;
 		Command cmd = parser.interpretCommand(command);
@@ -56,47 +61,50 @@ public class Logic {
 		logger.info("saving tasks to file.");
 		return feedback;
 	}
-	
-	/**
-	 * This method should be called to get all timing clashes with a particular task.
-	 * @param task The task you want to check clashes for.
-	 * @return A list of tasks that clash with this task, along with the task itself.
-	 */
-	public ArrayList<Task> getClashes (Task task){
-	    logger.info("getting all tasks");
-        ArrayList<Task> openTasks = storageConnector.getOpenList();
-        logger.info("initiating arraylist");
-        ArrayList<Task> tasksClashing = new ArrayList<Task>();
-        logger.info("adding input task to arraylist");
-        tasksClashing.add(task);
-        logger.info("checking if main task is an event");
-        if (!(task instanceof EventTask)){
-            logger.info("main task not event. returning task without checking for clashes.");
-            return tasksClashing;
-        }
-        logger.info("task is event. looping through all lists to check for clash");
-        for (Task taskUnderConsideration : openTasks){
-            addIfClashing(tasksClashing, task, taskUnderConsideration);
-        }
-        return tasksClashing;
-    }
-    
-	//adds taskUnderConsideration to the ArrayList if it clashes with 'task'
-    private void addIfClashing(ArrayList<Task> tasksClashing, Task task, Task taskUnderConsideration){
-        logger.info("checking if Event Task");
-        if (taskUnderConsideration instanceof EventTask){
-            logger.info("Getting dates from task under consideration");
-            Calendar startDate = taskUnderConsideration.getStartDate();
-            Calendar endDate = taskUnderConsideration.getEndDate();
-            logger.info("Adding to list if clashing");
-            if (endDate.compareTo(task.getStartDate()) > 0){
-                tasksClashing.add(taskUnderConsideration);
-            } else if (startDate.compareTo(task.getEndDate()) < 0){
-                tasksClashing.add(taskUnderConsideration);
-            }
-        }
-    }
 
+	/**
+	 * This method should be called to get all timing clashes with a particular
+	 * task.
+	 * 
+	 * @param task
+	 *            The task you want to check clashes for.
+	 * @return A list of tasks that clash with this task, along with the task
+	 *         itself.
+	 */
+	public ArrayList<Task> getClashes(Task task) {
+		logger.info("getting all tasks");
+		ArrayList<Task> openTasks = storageConnector.getOpenList();
+		logger.info("initiating arraylist");
+		ArrayList<Task> tasksClashing = new ArrayList<Task>();
+		logger.info("adding input task to arraylist");
+		tasksClashing.add(task);
+		logger.info("checking if main task is an event");
+		if (!(task instanceof EventTask)) {
+			logger.info("main task not event. returning task without checking for clashes.");
+			return tasksClashing;
+		}
+		logger.info("task is event. looping through all lists to check for clash");
+		for (Task taskUnderConsideration : openTasks) {
+			addIfClashing(tasksClashing, task, taskUnderConsideration);
+		}
+		return tasksClashing;
+	}
+
+	// adds taskUnderConsideration to the ArrayList if it clashes with 'task'
+	private void addIfClashing(ArrayList<Task> tasksClashing, Task task, Task taskUnderConsideration) {
+		logger.info("checking if Event Task");
+		if (taskUnderConsideration instanceof EventTask) {
+			logger.info("Getting dates from task under consideration");
+			Calendar startDate = taskUnderConsideration.getStartDate();
+			Calendar endDate = taskUnderConsideration.getEndDate();
+			logger.info("Adding to list if clashing");
+			if (endDate.compareTo(task.getStartDate()) > 0) {
+				tasksClashing.add(taskUnderConsideration);
+			} else if (startDate.compareTo(task.getEndDate()) < 0) {
+				tasksClashing.add(taskUnderConsideration);
+			}
+		}
+	}
 
 	// @@author A0125417L
 	// Sets directory of files
@@ -138,13 +146,29 @@ public class Logic {
 	// Counts number of overdue tasks
 	private int countOverdue(int overdueCount, ArrayList<Task> taskList, Calendar cal) {
 		for (Task task : taskList) {
-			if (task.getEndDate() != null) {
-				if (task.getEndDate().getTime().compareTo(cal.getTime()) < COMPARE_OVERDUE_VARIABLE) {
-					overdueCount++;
+			if (!(task instanceof EventTask)) {
+				assert (task.getEndDate() != null);
+				if (task.getEndDate() != null) {
+					if (task.getEndDate().getTime().compareTo(cal.getTime()) < 0) {
+						overdueCount++;
+					}
 				}
+			} else {
+				assert (task.getStartDate() != null);
+				if (task.getStartDate() != null) {
+					if (task.getStartDate().getTime().compareTo(cal.getTime()) < 0) {
+						overdueCount++;
+					}
+				}
+				// if (task.getEndDate() != null) {
+				// if (task.getEndDate().getTime().compareTo(cal.getTime()) <
+				// COMPARE_OVERDUE_VARIABLE) {
+				// overdueCount++;
+				// }
+				// }
 			}
+
 		}
 		return overdueCount;
 	}
-
 }
